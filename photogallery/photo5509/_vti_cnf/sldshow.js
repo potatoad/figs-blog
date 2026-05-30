15732edc14e90e6ccbcb5981b1a82bba92548a91
Fiona Margrie <fiona@pmafilmtv.com>
2026-05-30 16:58:13 +0100

    Updated about page

--- photogallery/photo5509/_vti_cnf/sldshow.js
+++ photogallery/photo5509/_vti_cnf/sldshow.js
@@ -1,12 +1,13 @@
 vti_encoding:SR|utf8-nl
-vti_timelastmodified:TW|30 May 2026 11:31:09 -0000
+vti_timelastmodified:TW|30 May 2026 12:01:11 -0000
 vti_author:SR|FIG-BA74D7B40B4\\Fig
 vti_modifiedby:SR|FIG-BA74D7B40B4\\Fig
-vti_nexttolasttimemodified:TW|30 May 2026 11:31:09 -0000
+vti_nexttolasttimemodified:TW|30 May 2026 12:01:11 -0000
 vti_timecreated:TR|30 May 2026 12:08:22 -0000
 vti_extenderversion:SR|5.0.2.2623
-vti_syncwith_localhost\\c\:\\documents and settings\\fig\\my documents\\my webs\\fig's site/c\:/documents and settings/fig/my documents/my webs/fig's site:TW|30 May 2026 11:31:09 -0000
+vti_syncwith_localhost\\c\:\\documents and settings\\fig\\my documents\\my webs\\fig's site/c\:/documents and settings/fig/my documents/my webs/fig's site:TW|30 May 2026 12:01:11 -0000
 vti_lineageid:SR|{E1E38223-A177-4A2A-BAE3-260F778E9201}
+vti_backlinkinfo:VX|photogallery/photo5509/real.htm
+vti_syncwith_localhost\\\\\\figbox\\fiona\\windows xp///figbox/fiona/windows xp:TW|30 May 2026 11:31:09 -0000
 vti_cacheddtm:TX|30 May 2026 12:08:48 -0000
 vti_filesize:IR|4362
-vti_backlinkinfo:VX|photogallery/photo5509/real.htm
